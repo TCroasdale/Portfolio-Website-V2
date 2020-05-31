7e807831804f75809339d82175c0b691104fedf5
Tom Croasdale <TCroasdale@users.noreply.github.com>
2020-05-31 13:11:22 +0100

    Switched to css spinner

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="fullscreen-bg" v-bind:class="{ hidden: !isLoading }">
-    <Icon class="spinner" icon="cog" />
+    <div class="y-center">
+        <div class="x-center">
+            <div class="spinner">
+                <div class="cog-circle">
+                    <div class="cog-hole"></div>
+                    <div class="cog-tooth"></div>
+                    <div class="cog-tooth"></div>
+                    <div class="cog-tooth"></div>
+
+                    <div class="cog-tooth"></div>
+                    <div class="cog-tooth"></div>
+                    <div class="cog-tooth"></div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Icon from '~/components/Icon.vue'
-
 export default {
-  components: {
-      Icon
-  },
   props: {
       isLoading: Boolean
   }
@@ -38,13 +48,80 @@ export default {
 }
 
 @keyframes spin {
-    from {transform:translate(50vw, 50vh) scale(10) rotate(0deg);}
-    to {transform:translate(50vw, 50vh) scale(10) rotate(360deg);}
+    from {transform: translate(-50%, -50%) rotate(0deg);}
+    to {transform: translate(-50%, -50%) rotate(360deg);}
 }
 
-.fullscreen-bg > .spinner {
-    // width: 50%;
-    transform: translate(50vw, 50vh) scale(10);
+.fullscreen-bg .spinner {
+    display: block;
+    // margin: 0 auto;
+    width: 175px;
+    height: 175px;
+    transform: translate(-50%, -50%);
     animation: spin 4s infinite linear;
 }
+
+.cog-circle {
+    display: block;
+    background-color: $colour-primary-0;
+    width: 150px;
+    height: 150px;
+    border-radius: 75px;
+    z-index: 11;
+    transform: translate(8.3%, 8.3%);
+}
+
+.y-center {
+    transform: translateY(50vh);
+}
+
+.x-center {
+    transform: translateX(50vw);
+}
+
+.cog-hole {
+    display: block;
+    background-color: $colour-primary-3;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    z-index: 11;
+    transform: translate(100%, 100%);
+}
+
+.cog-tooth {
+    position: absolute;
+    // top: -15%;
+    left: 33%;
+    display: block;
+    background-color: $colour-primary-0;
+    width: 50px;
+    height: 50px;
+    z-index: 11;
+}
+
+.cog-tooth:nth-child(2) {
+    transform: translateY(150%);
+}
+
+.cog-tooth:nth-child(3) {
+    transform: rotate(60deg) translateY(150%);
+}
+
+.cog-tooth:nth-child(4) {
+    transform: rotate(120deg) translateY(150%);
+}
+
+.cog-tooth:nth-child(5) {
+    transform: translateY(-150%);
+}
+
+.cog-tooth:nth-child(6) {
+    transform: rotate(60deg) translateY(-150%);
+}
+
+.cog-tooth:nth-child(7) {
+    transform: rotate(120deg) translateY(-150%);
+}
+
 </style>
